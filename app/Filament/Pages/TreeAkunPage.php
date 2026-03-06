4b75c9f7c733237bf461f25149855d81c80cd18b
Santoso <33777569+sanenchan@users.noreply.github.com>
2026-03-06 08:24:12 +0700

    add HasPageShield trait to TreeAkunPage for enhanced access control

--- app/Filament/Pages/TreeAkunPage.php
+++ app/Filament/Pages/TreeAkunPage.php
@@ -4,11 +4,13 @@ namespace App\Filament\Pages;
 
 use App\Models\IndukAkun;
 
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 use BackedEnum;
 
 class TreeAkunPage extends Page
 {
+    use HasPageShield;
     protected static BackedEnum|string|null $navigationIcon = 'heroicon-o-document-chart-bar';
     protected string $view = 'filament.pages.tree-akun-page';
     protected static ?string $navigationLabel = 'Chart of Accounts';
