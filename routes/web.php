c2358ce08cb8e0d64f0717c3e6ca97d1a7b9e941
DianKartiko <auliakartiko28@gmail.com>
2026-06-06 10:57:36 +0700

    update controller and route web

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\NotaController;
+use App\Http\Controllers\NotaThermalController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/nota/{penjualan}/cetak', [NotaController::class, 'print'])
+    ->name('nota.cetak');
+
+Route::get('/nota/{penjualan}/cetakThermal', [NotaThermalController::class, 'print'])
+    ->name('nota.cetakThermal');
 Route::get('/', function () {
     return Redirect('/admin');
 });
